09f853de5b65679389322fed3ac84baf7d510c73
ws <2606209307@qq.com>
2023-10-31 20:47:28 +0800

    add config配置中心和消息总线

---
+++ cloud-config-center-3355/src/main/java/com/atguigu/springcloud/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xixibox
+ * @version 1.0
+ * @date 2023/10/31 15:32
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-center-3366/src/main/java/com/atguigu/springcloud/ConfigClientMain3366.java
@@ -0,0 +1,20 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author xixibox
+ * @version 1.0
+ * @date 2023/10/31 16:50
+ */
+@EnableEurekaClient
+@SpringBootApplication
+public class ConfigClientMain3366
+{
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClientMain3366.class,args);
+    }
+}
