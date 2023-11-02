0b05900950b2789e922806812ee243139c93aa07
ws <2606209307@qq.com>
2023-11-02 17:31:11 +0800

    add SpringcloudAlibaba的nacos的了解和注册

---
+++ cloudalibaba-provider-payment9001/src/main/java/com/ws/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,19 @@
+package com.ws.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xixibox
+ * @version 1.0
+ * @date 2023/11/2 16:17
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001
+{
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
