b304eb65c690d91691c20973079e2dbb38222481
ws <2606209307@qq.com>
2023-11-03 17:33:19 +0800

    add SpringcloudAlibaba的nacos的配置中心，命名空间、分组等，以及nacos持久化和集群的初步了解，明天配置nacos集群，

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/ws/springcloud/alibaba/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.ws.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author xixibox
+ * @version 1.0
+ * @date 2023/11/3 15:33
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class NacosConfigClientMain3377
+{
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
